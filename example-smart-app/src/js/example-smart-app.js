3b074debcd0f3f27a8f39610ffa9cea1242e53a9
sledgey <dsledge123@gmail.com>
2018-11-27 11:56:31 +0000

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -25,20 +25,6 @@
                   });
         $.when(pt, obv).fail(onError);
 
-/*
-*/
-        var med = smart.patient.api.fetchAll({
-                    type: 'Medication',
-                    query: {
-                      code: {
-                        $or: ['http://loinc.org|4461-0', 'http://loinc.org|4460-2'
-                             ]
-                      }
-                    }
-                  });
-
-        $.when(pt, med).fail(onError);
-
         $.when(pt, obv).done(function(patient, obv) {
           var byCodes = smart.byCodes(obv, 'code');
           var gender = patient.gender;
