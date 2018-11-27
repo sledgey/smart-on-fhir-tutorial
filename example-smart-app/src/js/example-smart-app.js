dd7b48083becbffd0c51257f94cc778b973e0f60
sledgey <dsledge123@gmail.com>
2018-11-27 09:36:43 +0000

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -88,12 +88,12 @@
         });
 
 /*
+*/
         $.when(pt, med).done(function(patient, med) {
           var warfarin = byCodes('4461-0');
 
-          p.warfarin = warfarin[0].dosage.text + ' -> ' + warfarin[0].dosage.route.text;
+          //p.warfarin = warfarin[0].dosage.text + ' -> ' + warfarin[0].dosage.route.text;
         });
-*/
 
       } else {
         onError();
